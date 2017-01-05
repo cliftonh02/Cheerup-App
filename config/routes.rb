be222f4d76e576247993cfe77f558ab85941216f
yourUsername <your_email@example.com>
2017-01-05 12:19:27 -0500

    Crud method works

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   resources :moods
   resources :users
 
-  root to: 'user#index'
+  root to: 'users#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
